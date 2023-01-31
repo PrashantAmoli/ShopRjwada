74d13fbdd6ec13a9c9e68da88edd33fdcfc63b3e
Prashant <prashantamoli2621@gmail.com>
2023-01-31 16:29:35 +0530

    QR + AR fix

--- src/components/layouts/AR.jsx
+++ src/components/layouts/AR.jsx
@@ -1,4 +1,4 @@
-import Script from 'next/script';
+// import Script from 'next/script';
 import '@google/model-viewer';
 
 const OPTIONS = {
@@ -19,12 +19,12 @@ const OPTIONS = {
 export default function AR({ product }) {
 	return (
 		<>
-			<Script
+			{/* <Script
 				src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
 				onLoad={() => console.log('Model Viewer Loaded')}
 				onReady={() => console.log('Model Viewer Ready')}
 				onError={() => console.log('Model Viewer Error')}
-			/>
+			/> */}
 
 			<div className="absolute top-16 md:top-20 lg:top-28 w-full min-h-[68vh] flex justify-center rounded-xl cursor-grab">
 				<model-viewer
@@ -32,7 +32,7 @@ export default function AR({ product }) {
 					ios-src=""
 					poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
 					alt="Asian Shoes 3D"
-					style={{ width: '100vw', height: '68vh' }}
+					style={{ width: '100vw', height: '68vh', outline: 'none' }}
 					ar-modes="webxr scene-viewer quick-look"
 					shadow-intensity="2"
 					touch-action="pan-y"
@@ -40,6 +40,7 @@ export default function AR({ product }) {
 					exposure="0.52"
 					camera-controls
 					auto-rotate
+					outline="none"
 					ar
 				>
 					{/* <button slot="ar-button" className="bg-gray-200 rounded-md p-2 absolute top-2 right-2 z-50">
